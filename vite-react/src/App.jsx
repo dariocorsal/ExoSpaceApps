b454e4ac5f81bb803276066bf15c7977f6519e64
dariocorsal <dariocscs@gmail.com>
2024-10-06 12:42:02 -0700

    added new pages and modified explore

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainMenu from "./pages/MainMenu.jsx";
-import Exploration from "./pages/Explore.jsx";
+import Explore from "./pages/Explore.jsx";
 import Quiz from "./pages/Quiz.jsx";
 import Paint from "./pages/Draw.jsx";
-import Exoplanets from "./pages/Exoplanets.jsx";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainMenu />} />
-        <Route path="/exploration" element={<Exploration />} />
+        <Route path="/explore" element={<Explore />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/paint" element={<Paint />} />
-        <Route path="/exoplanets" element={<Exoplanets />} />
       </Routes>
     </Router>
   );
--- vite-react/src/pages/Quiz.jsx
+++ vite-react/src/pages/Quiz.jsx
@@ -1,3 +1,34 @@
+import React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 export default function Quiz() {
-  return <h1>Quiz</h1>;
+  const navigate = useNavigate(); // Hook para navegar entre páginas
+
+  return (
+    <div className="bg-indigo-800 p-4 flex justify-between items-center">
+      <button
+        onClick={() => navigate("/explore")} // Cambia a la página de Explore
+        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full flex items-center"
+      >
+        <ChevronLeft size={24} className="mr-2" />
+        Back
+      </button>
+      <div className="flex items-center">
+        <img
+          src="./public/amongus.png"
+          alt="Exoplanet Explorer Logo"
+          className="h-8 w-8 mr-2"
+        />
+        <h1 className="text-2xl font-bold">Fun with Exoplanets</h1>
+      </div>
+      <button
+        onClick={() => navigate("/exoplanets")} // Cambia a la página de Draw
+        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full flex items-center"
+      >
+        Next
+        <ChevronRight size={24} className="ml-2" />
+      </button>
+    </div>
+  );
 }

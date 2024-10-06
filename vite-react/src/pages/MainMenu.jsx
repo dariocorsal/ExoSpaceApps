130cdb37b15be5aed4d8556ec0c14036fd91fd99
dariocorsal <dariocscs@gmail.com>
2024-10-06 15:07:15 -0700

    updated menu

--- vite-react/src/pages/MainMenu.jsx
+++ vite-react/src/pages/MainMenu.jsx
@@ -1,39 +1,14 @@
-import React, { useState } from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import React from "react";
 import StarryBackground from "/src/Estrellas";
 import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
 import "/src/App.css";
 
-const sections = [
-  {
-    title: "Exploration",
-    description: "¡Here you can learn everything about exoplanets!",
-    image: "./public/estrellas/Untitled_Artwork 1.PNG",
-  },
-  {
-    title: "QUIZ",
-    description: "Here you can test your knowledge on exoplanets",
-    image: "./public/estrellas/Untitled_Artwork 2.PNG",
-  },
-  {
-    title: "PAINT",
-    description:
-      "¡Here you can create your own artwork inspired by an exoplanet!",
-    image: "./public/estrellas/Untitled_Artwork 3.PNG",
-  },
-];
-
 export default function App() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
 
-  const nextSection = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % sections.length);
-  };
-
-  const prevSection = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex - 1 + sections.length) % sections.length
-    );
+  const goToSection = (route) => {
+    navigate(route);
   };
 
   return (
@@ -53,58 +28,75 @@ export default function App() {
       </div>
 
       <div className="relative w-full max-w-5xl flex justify-center items-center h-[70vh] z-10">
-        <button
-          onClick={prevSection}
-          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 p-2 rounded-full z-20 hover:bg-opacity-75 transition-all"
-          aria-label="Sección anterior"
-        >
-          <ChevronLeft className="w-8 h-8 text-black" />
-        </button>
-
-        <div className="flex justify-center items-center w-full h-full overflow-hidden">
+        <div className="flex justify-center items-center w-full h-full">
           <motion.div
-            className="flex"
-            style={{ display: "flex", alignItems: "center" }}
-            initial={{ x: 0 }}
-            animate={{ x: `-${currentIndex * 100}%` }}
-            transition={{ duration: 0.5 }}
+            className="bg-white bg-opacity-80 rounded-3xl p-6 flex flex-col justify-between items-center m-2 transition-all duration-500"
+            initial={{ opacity: 0.5 }}
+            whileHover={{ scale: 1.05, opacity: 1 }}
           >
-            {sections.map((section, index) => (
-              <motion.div
-                key={section.title}
-                className={`bg-white bg-opacity-80 rounded-3xl p-6 flex flex-col justify-between items-center m-2 transition-all duration-500`}
-                style={{
-                  transform: `scale(${index === currentIndex ? 1 : 0.8})`,
-                  opacity: index === currentIndex ? 1 : 0.5,
-                }}
-                whileHover={{ scale: 1.05 }}
-              >
-                <img
-                  src={section.image}
-                  alt={section.title}
-                  className="w-full h-56 md:h-64 object-cover rounded-2xl mb-4"
-                />
-                <h2 className="text-2xl font-bold text-[#0f028f] mb-2">
-                  {section.title}
-                </h2>
-                <p className="text-gray-700 text-center mb-4">
-                  {section.description}
-                </p>
-                <button className="bg-[#ffa709] hover:bg-[#ff8c00] text-white font-bold py-2 px-4 rounded-full text-lg transition-colors">
-                  Go!
-                </button>
-              </motion.div>
-            ))}
+            <img
+              src="./public/estrellas/Untitled_Artwork 1.PNG"
+              alt="Exploration"
+              className="w-full h-56 md:h-64 object-cover rounded-2xl mb-4"
+            />
+            <h2 className="text-2xl font-bold text-[#0f028f] mb-2">
+              Exploration
+            </h2>
+            <p className="text-gray-700 text-center mb-4">
+              ¡Here you can learn everything about exoplanets!
+            </p>
+            <button
+              className="bg-[#ffa709] hover:bg-[#ff8c00] text-white font-bold py-2 px-4 rounded-full text-lg transition-colors"
+              onClick={() => goToSection("/explore")}
+            >
+              Go!
+            </button>
+          </motion.div>
+
+          <motion.div
+            className="bg-white bg-opacity-80 rounded-3xl p-6 flex flex-col justify-between items-center m-2 transition-all duration-500"
+            initial={{ opacity: 0.5 }}
+            whileHover={{ scale: 1.05, opacity: 1 }}
+          >
+            <img
+              src="./public/estrellas/Untitled_Artwork 2.PNG"
+              alt="QUIZ"
+              className="w-full h-56 md:h-64 object-cover rounded-2xl mb-4"
+            />
+            <h2 className="text-2xl font-bold text-[#0f028f] mb-2">QUIZ</h2>
+            <p className="text-gray-700 text-center mb-4">
+              Here you can test your knowledge on exoplanets
+            </p>
+            <button
+              className="bg-[#ffa709] hover:bg-[#ff8c00] text-white font-bold py-2 px-4 rounded-full text-lg transition-colors"
+              onClick={() => goToSection("/quiz")}
+            >
+              Go!
+            </button>
+          </motion.div>
+
+          <motion.div
+            className="bg-white bg-opacity-80 rounded-3xl p-6 flex flex-col justify-between items-center m-2 transition-all duration-500"
+            initial={{ opacity: 0.5 }}
+            whileHover={{ scale: 1.05, opacity: 1 }}
+          >
+            <img
+              src="./public/estrellas/Untitled_Artwork 3.PNG"
+              alt="PAINT"
+              className="w-full h-56 md:h-64 object-cover rounded-2xl mb-4"
+            />
+            <h2 className="text-2xl font-bold text-[#0f028f] mb-2">PAINT</h2>
+            <p className="text-gray-700 text-center mb-4">
+              ¡Here you can create your own artwork inspired by an exoplanet!
+            </p>
+            <button
+              className="bg-[#ffa709] hover:bg-[#ff8c00] text-white font-bold py-2 px-4 rounded-full text-lg transition-colors"
+              onClick={() => goToSection("/paint")}
+            >
+              Go!
+            </button>
           </motion.div>
         </div>
-
-        <button
-          onClick={nextSection}
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-50 p-2 rounded-full z-20 hover:bg-opacity-75 transition-all"
-          aria-label="Siguiente sección"
-        >
-          <ChevronRight className="w-8 h-8 text-black" />
-        </button>
       </div>
     </div>
   );
